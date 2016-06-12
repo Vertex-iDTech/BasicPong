34e4d2cbd3929170599f8493652afbf3a2827b27
iD Student <iD Student>
2016-06-12 14:00:27 -0700

    finishing touches

--- src/basicpong/BasicPong.java
+++ src/basicpong/BasicPong.java
@@ -7,13 +7,15 @@ import processing.core.PApplet;
 
 // A basic pong game. Can be customized by changing variables
 public class BasicPong extends PApplet {
-
+	
+	
+	
 	/************************* Change the variables here ***************************/
 	
 	// gameplay variables
 	int gameWidth  = 600;	// Width of the pong game (in pixels)
-	int gameHeight = 800;	// Height of the pong game (in pixels)
-	float speedX   = 6;		// Horizontal speed of ball
+	int gameHeight = 600;	// Height of the pong game (in pixels)
+	float speedX   = 4;		// Horizontal speed of ball
 	float speedY   = 3;     // Vertical speed of ball
 	int paddleSize = 150;   // Size of the paddle
 	
@@ -23,32 +25,41 @@ public class BasicPong extends PApplet {
 	Color ballColor       = Color.gray;
 	Color backgroundColor = Color.white;
 	
+	// characters and strings
+	String gameTitle  = "Pong";
+	char firstInitial = 'A';
+	char lastInitial  = 'E';
+	int fontSize      = 24;
+	Color fontColor   = Color.gray;
+	
 	/**************************    End of variables     *************************/
+	
+	
 	
 	// other variables, DON'T TOUCH!
 	float x = gameWidth/2;
 	float y = gameHeight/2; 
 	int diam;
 	
+	// sets up the game before first frame
 	public void setup() {
 		  size(gameWidth, gameHeight);
 		  noStroke();
 		  smooth();
 		  ellipseMode(CENTER);
+		  textAlign(CENTER);
+		  textSize(fontSize);
 	}
 
+	// runs once per frame. Updates state of the game and draws game to screen.
 	public void draw() {
+		
+		// draw background and game/creator name
 		background(backgroundColor.getRGB());
+		fill(fontColor.getRGB());
+		text(gameTitle + " by: " + firstInitial + ". " + lastInitial + ".", gameWidth/2, gameHeight/8);
 
-		fill(ballColor.getRGB());
-		diam = 20;
-		ellipse(x, y, diam, diam);
-
-		fill(wallColor.getRGB());
-		rect(0, 0, 20, height);
-		fill(paddleColor.getRGB());
-		rect(width - 30, mouseY - paddleSize / 2, 10, paddleSize);
-
+		// update the ball's location
 		x = x + speedX;
 		y = y + speedY;
 
@@ -76,5 +87,16 @@ public class BasicPong extends PApplet {
 			speedY = speedY * -1;
 			y = y + speedY;
 		}
+		
+		// draw the ball
+		fill(ballColor.getRGB());
+		diam = 20;
+		ellipse(x, y, diam, diam);
+
+		// draw the wall
+		fill(wallColor.getRGB());
+		rect(0, 0, 20, height);
+		fill(paddleColor.getRGB());
+		rect(width - 30, mouseY - paddleSize / 2, 10, paddleSize);
 	}
 }
